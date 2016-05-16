b053482defaa8e443388b81da1d74281f2877210
imorenodev@gmail.com <imorenodev@gmail.com>
2016-05-16 16:22:50 -0400

    added tests to practice with immutableJS data structures, Map and List

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,4 +1,5 @@
-import {expect} from 'chai';
+import { expect } from 'chai';
+import { List, Map } from 'immutable';
 
 describe('immutability', () => {
   describe('a number', () => {
@@ -11,6 +12,48 @@ describe('immutability', () => {
 
       expect(nextState).to.equal(43);
       expect(state).to.equal(42);
+    });
+  });
+  describe('A List', () => {
+    function addMovie (currentState, movie) {
+      return currentState.push(movie);
+    }
+
+    it('is immutable', () => {
+      let state = List.of('Trainspotting', '28 Days Later');
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later',
+        'Sunshine'
+      ));
+      expect(state).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later'
+      ));
+    });
+  });
+  describe('A Tree', () => {
+    function addMovie (currentState, movie) {
+      return currentState.set(
+        'movies',
+        currentState.get('movies').push(movie)
+      );
+    }
+
+    it('is immutable', () => {
+      let state = Map({
+        movies: List.of('Trainspotting', '28 Days Later')
+      });
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(state).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later'
+        )
+      }));
     });
   });
 });
